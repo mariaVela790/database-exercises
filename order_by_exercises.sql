ea6aa08422cc13ac13e40f7c5429224131be130a
Maria Vela <maria.vela790@gmail.com>
2018-12-03 12:26:56 -0600

    added order exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,38 @@
+use employees;
+
+describe employees;
+
+# select * from employees
+# where first_name in ('Irena', 'Vidya', 'Maya');
+select * from employees
+where (first_name = 'Irena'
+         or first_name = 'Vidya'
+         or first_name = 'Maya')
+order by last_name, first_name;
+
+select * from employees
+where (first_name = 'Irena'
+         or first_name = 'Vidya'
+         or first_name = 'Maya')
+  and gender = 'M';
+
+
+select * from employees
+where last_name like 'E%'
+order by emp_no desc;
+
+select * from employees
+where last_name like 'E%' or last_name like '%e';
+
+select * from employees
+where hire_date like '199%';
+#
+# select * from employees
+# where birth_date like '%-12-25';
+select * from employees
+where birth_date like '%12-25' && hire_date like '199%-%-%'
+order by birth_date, hire_date desc;
+
+select * from employees
+where last_name like '%q%' and last_name not like '%qu%';
+
